13bb2510ab2f704ba155852c76b395d981d68812
Rollie Parrish <rparrish@flightweb.com>
2025-10-20 11:13:08 -0700

    cleaned up md_connect

--- md_connect.R
+++ md_connect.R
@@ -3,18 +3,17 @@
 md_connect <- function(config_env = 'default') {
     
     config <- config::get(config = config_env)
+    #token <- Sys.getenv("MD_TOKEN")
+    token <- config$md_token
     
-    #conn <- DBI::dbConnect(duckdb::duckdb(), "Data/ccdm_dev.db")
     conn <- DBI::dbConnect(duckdb::duckdb(), ":memory:")
     
     # Authenticate with MotherDuck Token 
     DBI::dbExecute(conn, "INSTALL motherduck")
-    DBI::dbExecute(conn, "LOAD 'motherduck'")
+    DBI::dbExecute(conn, "LOAD motherduck")
     
-    if (config$md_token != "") { 
-        auth_query <- paste0("SET motherduck_token='", config$md_token, "';")
-        dbExecute(conn, auth_query)
-        }
+    auth_query <- paste0("SET motherduck_token='", token, "';")
+    dbExecute(conn, auth_query)
     
     # Attach to MotherDuck 
     DBI::dbExecute(conn, "ATTACH 'md:'")
